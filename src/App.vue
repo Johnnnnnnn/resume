53c5ad28f2979778bc036b8011c8c6171304ba07
unknown <flk09842@kloap.com>
2018-02-02 00:59:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+       {{firstName}}
+    </div>
+</template>
+
+<script>
+import resume from './data/resume'
+
+export default {
+    data : () => resume,
+    components: {
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/data/resume.js
@@ -0,0 +1,21 @@
+export default {
+    "firstName": "resume",
+    "lastName" : "",
+    "cellPhone" : "",
+    "email" : "",
+    "birthday" : "",
+    "gender" : "",
+    "education" : [{
+    	"school" : "",
+    	"startTime" : "",
+    	"endTime" : ""
+    }],
+    "experience" : [{
+    	"company" : "",
+    	"title" : "",
+    	"startTime" : "",
+    	"endTime" : "",
+    	"description" : ""
+    }]
+
+}
